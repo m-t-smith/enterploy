d4405c15be4f428e7f6de7c08c1c97091fdc76e9
m-t-smith <36452516+m-t-smith@users.noreply.github.com>
2018-11-29 17:50:58 -0500

    Added files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>enterploy</span>
+        <span class="font-weight-light"> Database Manager</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/m-t-smith"
+        target="_blank"
+      >
+        <span class="mr-2">developer</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      // 
+    }
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/EnterployLogo.png')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          enterploy
+        </h1>
+        <p class="subheading font-weight-regular">
+        Enterprise Employee Management  
+        </p>
+      </v-flex>
+      
+      <v-flex xs12>
+         <v-text-field
+      v-model = "mode"
+      disabled
+      readonly
+      ></v-text-field>
+      </v-flex>
+      
+      <v-flex xs12>
+          <v-text-field
+            outline
+            v-model = "input"
+            @change = test()
+          ></v-text-field>
+        </v-flex>
+        
+        <v-flex xs12 mb-4>
+          <v-btn outline>
+          Query
+          </v-btn> 
+          
+          <v-btn 
+          outline
+          
+          >
+            Insert
+          </v-btn> 
+          
+          <v-btn outline>
+          Delete
+          </v-btn> 
+        </v-flex>
+      
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      input: '',
+      mode: 'Query'
+    }),
+    methods: {
+      test () {
+        console.log(this.input)
+      },
+      xmode (newMode) {
+        data.mode = newMode
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
